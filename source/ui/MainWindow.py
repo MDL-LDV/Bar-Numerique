22a921e7f7ff49281b6d63156a2dc2ae623fedd7
Elie RUGGIERO <elie.ruggiero25@gmail.com>
2025-01-27 10:19:17 +0100

    final update

--- source/ui/MainWindow.py
+++ source/ui/MainWindow.py
@@ -1,9 +1,11 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QStackedWidget, QSplitter,\
-    QMenu, QPushButton
+from PySide6.QtWidgets import (QMainWindow, QWidget, QSplitter, QMenu, 
+    QSizePolicy)
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction, QPixmap
 
 from .QNavigationBar import QNavigationBar
+from .QBody import QBody
+from .Caisse import CaissePage
 
 from core.QtAddOns import QListWidgetItemId
 
@@ -34,6 +36,7 @@ class MainWindow(QMainWindow):
         # Give the app a name
         self.setWindowTitle("Bar numérique")
         self.resize(700, 400)
+        self.setStyleSheet("background-color: white;")
 
         # Définit l'icone dans la bar des tâches (en utilisant celle de 
         # l'application) https://stackoverflow.com/a/1552105/15793884
@@ -41,15 +44,20 @@ class MainWindow(QMainWindow):
             f"{self.windowTitle()}_{version}"
         )
 
-        #! TODO: Styliser le menu pour qu'il soit plus grand (height) et que 
-        # l'icon du menu burger soit plus grande et facile à cliquer
         self.menu_bar = self.menuBar()
+        self.menu_bar.setStyleSheet("QMenu::item:selected { color: black; background-color: #D5D5D5; border: none; } QMenuBar::item:selected { background-color: #D5D5D5; } QMenuBar::item:selected#burger { background-color: transparent; }")
         burger = QPixmap("assets/burger.svg")
-        self.menu_burger = QAction()
+        self.menu_burger = QAction(self.menu_bar)
+        self.menu_burger.setObjectName("burger")
         self.menu_burger.setIcon(burger)
         self.menu_bar.addAction(self.menu_burger)
         self.fichier = QMenu(self.menu_bar)
         self.fichier.setTitle("Fichier")
+        self.quitter = QAction(self.fichier)
+        self.quitter.setText("Quitter")
+        self.quitter.setShortcut("Ctrl+Q")
+        self.quitter.triggered.connect(self.close)
+        self.fichier.addAction(self.quitter)
         self.menu_bar.addMenu(self.fichier)
         self.setMenuBar(self.menu_bar)
         
@@ -58,12 +66,9 @@ class MainWindow(QMainWindow):
         self.menu_burger.triggered.connect(self.navbar.activer)
 
         item = QListWidgetItemId()
-        item.setData(0, "Bonjour")
-        self.navbar.addItem(item, lambda: print("Bonjour"))
-
-        item = QListWidgetItemId()
-        item.setData(0, "Au revoir")
-        self.navbar.addItem(item, lambda: print("Au revoir"))
+        item.identifiant = "Caisse"
+        item.setData(0, "Caisse")
+        self.navbar.addItem(item, lambda: print("Caisse"))
 
         # self.caisse_widget = QWidget()
         # layout = QHBoxLayout(self.caisse_widget)
@@ -77,9 +82,12 @@ class MainWindow(QMainWindow):
         #     "QWidget:hover { background-color: yellow; } "
         #     + "QWidget { background-color: transparent; } ")
         # self.navbar.addWidget(self.caisse_widget, lambda: print("Au revoir"))
+        
+        self.body = QBody(self)
+        self.navbar.pageclicked.connect(self.body.dispatcher)
 
-        self.body = QStackedWidget(self)
-        self.body.setStyleSheet("background-color: red;")
+        self.caisse = CaissePage(self.body)
+        self.body.addWidget(self.caisse)
         
         self.splitter = QSplitter(self)
         self.splitter.insertWidget(0, self.navbar)
