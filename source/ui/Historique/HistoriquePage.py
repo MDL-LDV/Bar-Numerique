072cab2980b335a617a61f4a600c55f8fb7ac9e6
Elie RUGGIERO <elie.ruggiero25@gmail.com>
2025-02-22 14:49:19 +0100

    Update HistoriquePage.py

--- source/ui/Historique/HistoriquePage.py
+++ source/ui/Historique/HistoriquePage.py
@@ -2,11 +2,17 @@ from __future__ import annotations
 
 from PySide6.QtWidgets import QWidget, QFrame
 
-from typing import Optional
+class Commande(QFrame):
+    def __init__(self: Commande, parent: QWidget) -> None:
+        super().__init__(parent)
+
+class ListeCommandes(QFrame):
+    def __init__(self: ListeCommandes, parent: QWidget) -> None:
+        super().__init__(parent)
 
 
 class HistoriquePage(QFrame):
-    def __init__(self: HistoriquePage, parent: Optional[QWidget] = None) -> None:
+    def __init__(self: HistoriquePage, parent: QWidget) -> None:
         super().__init__(parent)
         self.setObjectName("Historique")
         
